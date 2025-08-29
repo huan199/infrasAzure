2a679369370d66bd34b738f84fcd31e0da7cc1a3
huannn <huanhit199@gmail.com>
2025-08-29 15:55:49 +0700

    Update enviroment dev test

--- application/webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
+++ application/webapp/obj/Debug/net8.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf7422c12fa4b026c849fff036d8ef067e4e7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3c8fc35b8ff14c4a7f572b12ee4d5b2c6aa827")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
